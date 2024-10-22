8cac26e6ea9ef861fd056c51bbeeb60af207c83b
Aryan <r0984834@student.thomasmore.be>
2024-10-22 15:44:27 +0200

    Mini project upload

---
+++ BGRemove.py
@@ -0,0 +1,43 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+image_path = input("Your image path:")
+
+allowed = ['jpg', 'png', 'webp', 'jpeg', 'gif']
+
+isValidFormat = False
+isValidStructure = False
+format = ""
+
+if "." in image_path:
+    isValidStructure = True
+    format = image_path.split(".")[1]
+
+if isValidStructure == False:
+    print("Image path is not structured correctly!")
+
+else:
+    for item in allowed:
+        if item == format:
+            isValidFormat = True
+            break
+
+    if isValidFormat == False:
+        print("Image has to be in .jpg/.png/.webp/.jpeg/.gif format!")
+
+    else:
+        response = requests.post(
+            'https://api.remove.bg/v1.0/removebg',
+            files={'image_file': open('2zg7jkerkkjb1.jpg', 'rb')},
+            data={'size': 'auto'},
+            headers={'X-Api-Key': api_key},
+        )
+
+        if response.status_code == requests.codes.ok:
+            with open('no-bg.png', 'wb') as out:
+                out.write(response.content)
+        else:
+            print("Error:", response.status_code, response.text)
---
+++ README.md
@@ -0,0 +1,4 @@
+# Background removal tool using RemoveBG API (Python)
+- Remove background of any image using RemoveBG API
+- Quick and simple background removal tool built in Python
+- I couldn't think of any more points but wanted to round it up to 3 points so act like there's something mind blowing written here.
